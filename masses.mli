ffbc2d1cb2b2a5b709fff80291b57216610bd5ab
Samuel Green <green.sam1192@gmail.com>
2017-04-05 20:38:48 -0400

    initial deploy

---
+++ masses.mli
@@ -0,0 +1,59 @@
+(*
+		       Point masses with forces
+			  CS51 Problem Set 7
+			 -.-. ... ..... .----
+ *)
+
+open Points
+       
+val cFRAMESIZE : int
+
+(* Class: mass 
+     Arguments: initx : float
+                inity : float      initial x and y positions of the mass
+		            m : float          the "physical" mass of the mass for computing 
+		    		                        force effects 
+
+   A mass is a point that has a mass associated with it and can have
+   forces act upon it. *)
+		   
+class mass : float -> float -> float -> object
+  (* A mass is located at a point *)
+  inherit point 
+
+  (* Masses have distinct integer ids *)
+  method get_id : int
+
+  (* The mass itself *)
+  val mass : float
+
+  (* Moves mass to provided point, storing previous position for
+     possible later restoring, clipping to frame boundary while we're
+     at it. *)
+  method move : point -> unit
+	   
+  (* Restore position of mass to last stored position *)
+  method restore_pos : unit
+							  
+  (* Forces on the mass *)
+  (* Sets force on the mass to the vector given by point *)
+  method set_force : point -> unit
+  (* Resets the force on the mass to 0 *)
+  method reset_force : unit
+  (* Returns the force on the mass *)
+  method get_force : point
+  (* Adds to the existing force on the mass a force specified as the
+     given point vector *)
+  method add_force : point -> unit
+  (* Applies the current force to the mass, moving it accordingly, and
+     resetting the force to 0 *)
+  method apply_force : unit
+  (* Scales the current force by the provided multiplicative factor *)
+  method scale_force : float -> unit
+
+  (* I/O methods *)
+  (* Draws an indicator of the mass location on the canvas *)
+  method reveal : unit
+  (* Prints a textual description of the object on stdout *)
+  method describe : unit
+end
---
+++ testuniformcentered.ml
@@ -0,0 +1,55 @@
+(*
+                   A sample force-directed graphic:
+                 UNIFORM CENTERED PLACEMENT OF NODES
+                          CS51 Problem Set 7
+                         -.-. ... ..... .----
+ *)
+
+open List ;;
+
+open Points ;;
+open Masses ;;
+open Controls ;;
+open Graphobj ;;
+open Graphdraw ;;
+
+let uniformcentered n =
+  Random.init 0;
+  (* masses:
+        place n masses at random locations within the frame *)
+  let masses = map (fun _i -> new mass
+                                 (Random.float (float_of_int cFRAMESIZE))
+                                 (Random.float (float_of_int cFRAMESIZE))
+                                 1.)
+                   (CS51.range 1 n) in
+  (* constraints: 
+        each mass is tied by postitionspring to the center of the frame
+        each pair of masses has a logisticrepel to all others
+   *)
+  let constraints =
+    let centerpoint =
+      new point (float_of_int cFRAMESIZE/.2.) (float_of_int cFRAMESIZE/.2.) in
+    (flatten (flatten
+                (mapi (fun i mi ->
+                       mapi (fun j mj ->
+                             if i > j then []
+                             else if i = j then [((new positionspring
+                                                       ~stiffness: (CS51.const 0.001)
+                                                       mi centerpoint) :> control)]
+                             else [((new logisticrepel
+                                         ~rest: 50.
+                                         mi mj) :> control)])
+                            masses)
+                      masses)) :> control list) in
+  (* scene: all masses are shown as light gray circles *)
+  let scene =
+    map (fun m -> new circle
+                      ~label:(string_of_int m#get_id)
+                      ~col: cLIGHTGRAY
+                      ~textcol: Graphics.black
+                      (m :> point) 10)
+        masses in
+  x11_solve masses constraints scene
+;;
+
+let _ =  uniformcentered 12 ;;

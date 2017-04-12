ddf4b83c1d4d5d6f26aad53e32af51394bf653a1
mattj26 <matthewjacobson26@gmail.com>
2017-04-12 04:27:59 -0400

    Done with square and rect

--- graphobj.ml
+++ graphobj.ml
@@ -1,4 +1,4 @@
-(* 
+(*
                           Graphical objects
                           CS51 Problem Set 7
                          -.-. ... ..... .----
@@ -10,12 +10,12 @@ open Printf ;;
 open Points ;;
 
 (*......................................................................
-  Constants -- for configuring the graphical output 
+  Constants -- for configuring the graphical output
  *)
 
 (* Linewidth to use for edges and object borders*)
 let cLINEWIDTH = 3 ;;
-  
+
 (* Some useful colors. See color type in Graphics module. *)
 let cBLACK = 0x000000 ;;
 let cDARKGRAY = 0x555555 ;;
@@ -23,7 +23,7 @@ let cGRAY = 0x999999 ;;
 let cLIGHTGRAY = 0xCCCCCC ;;
 
 (*......................................................................
-  Utility functions 
+  Utility functions
  *)
 
 (* draw_text_centered s (x, y) -- Draws the text in string s such that
@@ -41,7 +41,7 @@ let draw_triangle (pt : point) (dir : point) (scale : float) : unit =
   let left = (pt#minus (dir#scale scale))#plus (ortho#scale (scale /. 2.0)) in
   let right = (pt#minus (dir#scale scale))#plus (ortho#scale (-. scale /. 2.0)) in
   fill_poly [| pt#round; left#round; right#round |] ;;
-                                                 
+
 (* Minimum and maximum elements in a list *)
 let minimum = CS51.reduce min ;;
 let maximum = CS51.reduce max ;;
@@ -59,9 +59,9 @@ have additional elements.
 The layer indicates the drawing order, with lower-numbered layers
 "further away than" (potentially obscured by) higher numbered
 layers. By default, nodes are at layer 20 (up front), with edges at
-layer 10 behind them, and zone boxes at layer 0 even further back. 
+layer 10 behind them, and zone boxes at layer 0 even further back.
  *)
-  
+
 class virtual drawable ?(label : string = "")
                        ?(layer : int = 0)
                        (col : color) =
@@ -75,7 +75,7 @@ class virtual drawable ?(label : string = "")
 (* Here's an example graphical object -- a circle drawn at a specified
    point. We've provided the code for this one. You'll do the
    others. *)
-    
+
 (* Class circle -- nodes depicted with a small circle
    Arguments: ?label : string    optional label for the node (default: "")
               ?col : color       color to  draw the circle (black)
@@ -89,7 +89,7 @@ class circle ?(label : string = "")
              ?(col : color = black)
              ?(layer : int = 20)
              ?(textcol : color = red)
-             ?(linewidth : int = cLINEWIDTH) 
+             ?(linewidth : int = cLINEWIDTH)
              (m : point)
              (r : int) =
   object
@@ -98,7 +98,7 @@ class circle ?(label : string = "")
     val radius : int = r
     val textcolor : color = textcol
     val linewidth : int = linewidth
-                       
+
     method draw =
       let (x, y) as p = anchor#round in
       set_line_width linewidth;
@@ -115,8 +115,8 @@ class circle ?(label : string = "")
   Your part goes here: Provide the implementations of the rectangle,
   square, and edge classes.
 ......................................................................*)
-     
-(* Class rectangle -- nodes depicted with a small rectangle 
+
+(* Class rectangle -- nodes depicted with a small rectangle
    Arguments: ?label : string    optional label for the node (default: "")
               ?col : color       color to  draw the rectangle (black)
               ?layer : int       layer index (20)
@@ -126,8 +126,34 @@ class circle ?(label : string = "")
               w : int            width of the rectangle
               h : int            height of the rectangle
  *)
+ class rectangle ?(label : string = "")
+                 ?(col : color = black)
+                 ?(layer: int = 20)
+                 ?(textcol : color = red)
+                 ?(linew : int = cLINEWIDTH)
+                 (m : point)
+                 (w : int)
+                 (h : int) =
+  object
+    inherit drawable ~label ~layer col
+    val anchor : point = m
+    val textcolor : color = textcol
+    val linewidth : int = linew
+    val width : int = w
+    val height : int = h
 
-     
+    method draw =
+      let x, y = anchor#round in
+      set_line_width linewidth;
+      set_color background;
+      fill_rect x y width height;
+      set_color col;
+      draw_rect x y width height;
+      set_color textcol;
+      draw_text_centered label (x + linewidth / 2 + width / 2, y + height / 2)
+  end
+
+
 (* Class square -- nodes depicted with a small square
    Arguments: ?label : string    optional label for the node (default: "")
               ?col : color       color to  draw the square (black)
@@ -137,8 +163,21 @@ class circle ?(label : string = "")
               m : point          center of the square
               w : int            width of the square
  *)
-   
-    
+
+class square ?(label : string = "")
+                 ?(col : color = black)
+                 ?(layer: int = 20)
+                 ?(textcol : color = red)
+                 ?(linew : int = cLINEWIDTH)
+                 (m : point)
+                 (l : int)
+                 =
+  object
+    inherit rectangle ~label ~col ~layer ~textcol ~linew m l l
+  end
+
+
+
 (* Class edge -- an edge between two points
    Arguments: ?label : string    optional label for the edge (default: "")
               ?col : color       color to  draw the square (black)
@@ -148,13 +187,13 @@ class circle ?(label : string = "")
               source : point     source point of the edge
               target : point     target point of the edge
  *)
-   
-     
+
+
 (* Class zone -- a zone box that surrounds a set of points
    Arguments: ?label : string      optional label for the edge (default: "")
               ?col : color         color to  draw the square (black)
               ?layer : int         layer index (20)
-              ?border : int        amount of whitespace to leave around the 
+              ?border : int        amount of whitespace to leave around the
                                    bounding box of the enclosed masses
               ?linewidth : int     linewidth to draw the square (cLINEWIDTH)
               points : point list  a list of points defining the zone to be enclosed
@@ -178,6 +217,6 @@ class zone ?(col : color = cLIGHTGRAY)
         int_of_float (maximum (map (fun m -> snd m#pos) points)) + border in
       draw_rect xmin ymin (xmax - xmin) (ymax - ymin)
   end
-      
-      
-    
+
+
+
--- makefile
+++ makefile
@@ -1,11 +1,16 @@
+all: masses graphobj points
+
+clean:
+	ocamlbuild -clean
+
+graphobj:
+	ocamlbuild graphobj.byte
+
 masses:
 	ocamlbuild masses.byte
-
-massesT:
-	ocamlbuild masses_test.byte
 
 points:
 	ocamlbuild points.byte
 
-pointsT:
-	ocamlbuild points_tests.byte
+tests:
+	ocamlbuild tests.byte

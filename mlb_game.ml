e1b0901a36df10a39d384c794a1f1b2303477b99
mattj26 <matthewjacobson26@gmail.com>
2017-04-13 14:56:43 -0400

    My graph done

--- mlb_game.ml
+++ mlb_game.ml
@@ -1,21 +1,81 @@
-open Masses;;
-open Points;;
+open List ;;
 
-type player =
-{
-  home : bool;
-  name : string;
-  hits : int list
+open Points ;;
+open Masses ;;
+open Controls ;;
+open Graphobj ;;
+open Graphdraw ;;
+
+
+type graph =
+    { labels : string list; (* labels, one for each node *)
+      positions : (int * int) list; (* initial x, y positions of reach node *)
+      edges : (int * int) list (* edges between nodes given as pairs of node indices *)
+    } ;;
+
+(* graph g -- Generates and solves a force-directed graph layout for
+   the given graph g. *)
+
+let graph ({labels; positions; edges} : graph) : unit =
+
+  let masses = positions |> map (fun (x, y) ->
+                                 new mass (float_of_int x) (float_of_int y) 1.) in
+
+  let scene = masses |> mapi (fun i m ->
+                              new square ~label: (nth labels i)
+                                  ~col: cGRAY
+                                  ~textcol: cBLACK
+                                  (m :> point) 20) in
+
+  let edgeobjs = edges |> map (fun (i, j) ->
+                               new edge ((nth masses i) :> point)
+                                   ~col: cLIGHTGRAY
+                                   ((nth masses j) :> point)) in
+
+  let constraints =
+    let centerpoint = new point (float_of_int cFRAMESIZE/.2.) (float_of_int cFRAMESIZE/.2.) in
+    flatten (flatten (mapi (fun i mi ->
+                            mapi (fun j mj ->
+                                  if i = j then
+                                    (* pull all nodes toward center *)
+                                    [((new positionspring
+                                                       ~stiffness: (CS51.const 0.008)
+                                                       mi centerpoint) :> control)]
+                                  else if mem (i, j) edges then
+                                    (* connected nodes should be close *)
+                                    [((new bispring ~rest:80.
+                                           ~stiffness: (CS51.const (1. /. (float_of_int (length masses))))
+                                           mi mj) :> control)]
+                                  else if i < j then
+                                    (* nodes shouldn't overlap *)
+                                    [((new logisticrepel ~rest: 100.
+                                           ~stiffness: (CS51.const 1.0)
+                                           ~steepness: 0.5
+                                           mi mj) :> control)]
+                                  else [])
+                                 masses)
+                           masses))
+  in
+  x11_solve masses (constraints :> control list) (scene @ edgeobjs) ;;
+
+let mets_phils () =
+  graph { labels = ["Mets"; "Phillies"; "Conforto"; "Cabrera"; "Cespedes";
+                    "Bruce"; "Walker"; "Duda"; "Reyes"; "d'Arnaud"; "Wheeler";
+                    "Hernandez"; "Kendrick"; "Herrera"; "Franco"; "Saunders";
+                    "Joseph"; "Rupp"; "Galvis"; "Velasquez"];
+          positions = [(3,3); (2,2); (51,51); (51,50); (51, 49); (51,48);
+                      (51,47); (51,46); (51,45); (51,44); (51,43); (10, 9);
+                      (10, 8); (10, 7); (10, 6); (10, 5); (10, 4); (10, 3);
+                      (10, 2); (10, 1)];
+          edges = [(0,2); (0,3); (0,4); (0,5); (0,6); (0,7); (0,8); (0,9); (0,10);
+                    (1, 11); (1, 12); (1, 13); (1, 14); (1, 15); (1, 16);
+                    (1, 17); (1, 18); (1, 19)]
 }
-let mets = new point 1. 1.
-let phills = new point 2. 2.
-let teamPoints = [mets; phills]
 
-let metsMass = new mass mets#x mets#y 1.
-let phillsMass = new mass phills#x phills#y 1.
-
-let center = new point (float_of_int cFRAMESIZE /. 2.) (float_of_int cFRAMESIZE /. 2.)
+let _ = mets_phils ();;
 
 
 
-let mets1 = {home = false; name = "Conforto"; hits = [1; 4]}
+
+
+
